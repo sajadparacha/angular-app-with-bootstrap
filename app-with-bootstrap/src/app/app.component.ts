df23afe59a7921eecafde7e9ac37edd800221f37
Sajjad Ahmed Paracha <sajadparacha@gmail.com>
2020-08-01 09:32:52 +0300

    include and user javascrpt library (lodash)
    include and user javascrpt library (lodash)

--- app-with-bootstrap/src/app/app.component.ts
+++ app-with-bootstrap/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+//import 'lodash';
+import {random} from 'lodash';
+//declare var _:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-with-bootstrap';
+  randomNumber:any="Nill";
+
+  showRandomNumber():void{
+    this.randomNumber=random(1,20);
+  }
 }
